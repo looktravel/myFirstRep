aa8425cdaf7bbce37ccb54588e4d0484df24aade
looktravel <54234839+looktravel@users.noreply.github.com>
2022-01-10 15:36:21 +0300

    Add files via upload

--- style.js
+++ style.js
@@ -1,1 +1,23 @@
 // comment
+// УРОК 2
+// Задание 1
+let title = "myFirstProject";
+let screens  = "Простые, Сложные, Интерактивные";
+let screenPrice = 12345;
+let rollback  = 99;
+let fullPrice = 3000;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " гривен");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
+
+screens = screens.toLowerCase();
+console.log(screens.split(", "));
+
+console.log("Процент отката посреднику за работу: " + fullPrice * (rollback/100));
